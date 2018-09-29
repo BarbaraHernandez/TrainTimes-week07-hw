5e7910fc2ac784222c2f68f9e67648e2e0be399d
BarbaraHernandez <bandezanne@gmail.com>
2018-09-28 20:49:06 -0400

    added firebase

---
+++ assets/javascript/myjs.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDxervGDaIYj-YjnK_SDK-gGNrz4EgJdmI",
+    authDomain: "train-schedule-b1160.firebaseapp.com",
+    databaseURL: "https://train-schedule-b1160.firebaseio.com",
+    projectId: "train-schedule-b1160",
+    storageBucket: "train-schedule-b1160.appspot.com",
+    messagingSenderId: "876468237987"
+  };
+  firebase.initializeApp(config);
+  
+});
